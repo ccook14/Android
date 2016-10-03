81989c5daa0ae3683fd2992eb7fe9c7eaede18d1
Colin Cook <colin.cook@cox.net>
2016-10-03 16:58:42 -0500

    Update Prompter.java

--- Hangman/Prompter.java
+++ Hangman/Prompter.java
@@ -1,3 +1,23 @@
+import java.io.Console;
+
 public class Prompter {
-    
+    private Game mGame;
+	
+	
+	public Prompter(Game game) {
+		mGame = game;
+		
+	}
+	
+	public boolean promptForGuess() {
+		Console console = System.console();
+		String guessAsString = console.readLine("Enter a letter:  ");
+		char guess = guessAsString.charAt(0);
+		return mGame.applyGuess(guess);
+		
+	}
+	
+	public void displayProgress() {
+		System.out.printf("Try to solve:   %s\n", mGame.getCurrentProgress());
+	}
 }
