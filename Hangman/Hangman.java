4d1c68c8792c0cf61ba3cb0ff5ce44051351b57e
Colin Cook <colin.cook@cox.net>
2016-10-04 08:59:24 -0500

    Update Hangman.java

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -2,13 +2,8 @@ public class Hangman {
     public static void main(String[] args){
         Game game = new Game("Treehouse");
         Prompter prompter = new Prompter(game);
-        prompter.displayProgress();
-        boolean isHit = prompter.promptForGuess();
-        if (isHit) {
-        	System.out.println("We got a hit!");
-        } else {
-        	System.out.println("Whoops that was a miss");
-        }
-      prompter.displayProgress();  
+        prompter.play();
+        
+
     }
 }
